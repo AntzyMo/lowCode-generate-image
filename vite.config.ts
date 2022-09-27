2c45a4ac23e1ac25fa15d8c6fee069b329574f4e
AntzyMo <mozbano@163.com>
2022-09-27 18:01:35 +0800

    feat: 部署

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,10 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  /* 可以删除 只是用来配置git pages */
+  base: command === 'build' ? '/test-pictiue/' : '/',
+
   plugins: [
     vue({
       reactivityTransform: true
@@ -43,4 +46,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+}))
